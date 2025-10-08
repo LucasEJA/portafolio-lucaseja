82011d1a327001c2e214381742815d0c27d66ce9
Lucas <luja9575@gmail.com>
2025-10-08 15:56:14 -0500

    refactor, agregación de tecnologias usadas en los proyectos.

--- src/constants/projectsData.js
+++ src/constants/projectsData.js
@@ -8,7 +8,7 @@ export const PROJECTS_DATA = [
     projectName: "Sistema de gestión de archivos",
     description: "La aplicación web desarrollada permite gestionar las tareas administrativas de un instituto. Incluye funcionalidades como la carga y gestión de certificados y constancias, búsqueda de estudiantes, y generación automatizada de documentos.",
     driveLink: "https://drive.google.com/file/d/1SG74An3FbTJgwiQTz7QyhCY-MNoZ-5Nl/view?usp=sharing",
-    technologies: ["React", "Node.js", "MySQL", "Express"],
+    technologies: ["React", "Node.js", "MySQL", "Express", "Python"],
     category: "web-app",
     featured: true,
     status: "completed"
@@ -19,7 +19,7 @@ export const PROJECTS_DATA = [
     projectName: "Sistema interno de Polleria Burger Maicol",
     description: "La aplicación web agiliza la gestión de pedidos en la pollería conectando cajeros, chef, meseros y repartidores en un sistema integrado. Permite registro eficiente, actualización en tiempo real del estado de pedidos, notificaciones inmediatas y generación automática de reportes. Su flujo digitalizado reduce errores, mejora el servicio y optimiza la administración del negocio con una interfaz adaptada a cada rol.",
     driveLink: "https://drive.google.com/file/d/1eeRapJRw-sK038jTYfree7vw1dOddRst/view?usp=sharing",
-    technologies: ["React", "Node.js", "MySQL", "Socket.io"],
+    technologies: ["React", "Node.js", "MySQL", "Express", "Tailwind CSS", "Socket.io"],
     category: "web-app",
     featured: true,
     status: "completed"
