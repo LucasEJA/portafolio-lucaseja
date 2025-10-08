e2f0bebe27272e62252ac979411ecf67efdf5798
Lucas <luja9575@gmail.com>
2025-10-07 21:32:24 -0500

    fix -  case sensitivity en AboutMe component.

--- src/components/sections/AboutMe.jsx
+++ src/components/sections/AboutMe.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../styles/components/AboutMe.css';
+import '../../styles/components/Aboutme.css';
 
 export const AboutMe = () => {
   const handleCVClick = () => {
